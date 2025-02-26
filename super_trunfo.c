da2ee58894f7b268c3f3efb7874064f5e31c7b9e
rodrigolucas52 <rodrigo.lucas52@gmail.com>
2025-02-26 17:35:08 +0000

    Codigo super super_trunfo

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    char Estado;
-    char Código da Carta;
-    char cidade[];
+    char estado;
+    char codigo da Carta;
+    int cidade;
     int populacao;
-    float Área em km²;
+    float area enum km;
     float PIB;
+    int numeros de pontos turisticos;
+
+
+    printf("digite o estado: \n");
+
+
+    printf("Digite a cidade: \n");
+
+
+    printf("digite o codigo da carta: \n");
+
+    
+
+    return 0;
+
+
+
+
+
 }
